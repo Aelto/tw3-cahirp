68ae096b7b6c4f237143bad1a0067445712e8ed3
Aelto <Aelto@users.noreply.github.com>
2023-10-06 18:03:12 +0200

    match surrounding indent on emitted code

--- src/codegen/emitter.rs
+++ src/codegen/emitter.rs
@@ -13,12 +13,36 @@ pub trait CodeEmitter {
         let (left, right) = file.split_at_mut(cursor.pos.idx);
 
         let mut output = String::with_capacity(left.len() + code.len() + right.len());
-        output.push_str(left);
-        // TODO: it could be interesting to match the indentation of the previous
-        // lines on the code we insert here:
-        output.push_str(code);
+        output.push_str(left.trim_end_matches('\t').trim_end_matches(' '));
+        output.push_str(&match_line_indentation(code, left));
         output.push_str(&right[cursor.pos.selection_len..]);
 
         Ok(output)
     }
+}
+
+/// generates a string of code whose indentation matches the last line of
+/// `surrounding`
+fn match_line_indentation(code: &str, surrounding: &str) -> String {
+    let segment: String = surrounding
+        .chars()
+        .rev()
+        .skip_while(|c| c != &'\n')
+        .skip(1)
+        .take_while(|c| c != &'\n')
+        .collect();
+
+    // trim_end as it is reversed so it is actually the start of the line
+    let indents = &segment[segment.trim_end().len()..];
+
+    let mut output = String::new();
+    for line in code.lines() {
+        // for each emitted line we copy the exact indentation of the last
+        // line from the surrounding code:
+        output.push_str(indents);
+        output.push_str(line.trim());
+        output.push('\n');
+    }
+
+    output
 }
