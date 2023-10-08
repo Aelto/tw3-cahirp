a557f5a103681a154362d99f3fa378019a666fe0
Aelto <Aelto@users.noreply.github.com>
2023-10-08 14:21:19 +0200

    add utf16 support

--- src/encoding.rs
+++ src/encoding.rs
@@ -1,7 +1,29 @@
-use std::path::PathBuf;
+use std::{
+  fs::File,
+  io::{BufReader, ErrorKind},
+  path::PathBuf
+};
+
+use encoding_rs_io::DecodeReaderBytesBuilder;
 
 pub fn read_file(path: &PathBuf) -> std::io::Result<String> {
-  let contents = std::fs::read_to_string(path)?;
+  let contents = read_utf16_file(path)?;
+  // let contents = std::fs::read_to_string(path).or_else(|_| read_utf16_file(path))?;
 
   Ok(contents.replace("\r", ""))
+}
+
+fn read_utf16_file(path: &PathBuf) -> std::io::Result<String> {
+  use std::io::Read;
+
+  let file = File::open(path)?;
+  let source = BufReader::new(file);
+
+  let mut decoder = DecodeReaderBytesBuilder::new().build(source);
+  let mut dest = String::new();
+  // decoder implements the io::Read trait, so it can easily be plugged
+  // into any consumer expecting an arbitrary reader.
+  decoder.read_to_string(&mut dest)?;
+
+  Ok(dest)
 }
